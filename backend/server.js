1553e185b526ed49b0b7e5746a4d83356a0a2e0a
Parth Goyal <mail.parthgoyal@gmail.com>
2025-08-17 23:26:31 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -11,16 +11,16 @@ app.use(helmet());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'false');
-  
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
-  
+
   next();
 });
+
 
 app.use(express.json({ limit: '10mb' }));
 
