9f4f87e1cd4df10fdc3e8884fd2e6cadc4a5d3a0
Parth Goyal <mail.parthgoyal@gmail.com>
2025-08-17 22:58:05 +0530

    Removed CORS

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 
@@ -9,21 +8,6 @@ const websiteRoutes = require('./src/routes/websiteRoutes');
 const app = express();
 
 app.use(helmet());
-
-const corsOptions = {
-  origin: [
-    'http://localhost:5173',
-    'https://weblytics-lyart.vercel.app'
-  ],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-  optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions));
-
-app.options('*', cors(corsOptions));
 
 app.use(express.json({ limit: '10mb' }));
 
